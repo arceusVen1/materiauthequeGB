77d1e2298ea3a878a85799cc22f990f6e8f75c7d
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-08-12 00:38:27 +0200

    famille form

--- backend/materiau/admin.py
+++ backend/materiau/admin.py
@@ -2,20 +2,35 @@ from django.contrib import admin
 from .models.materiau import Materiau
 from .models.sous_famille import SousFamille
 from .models.famille import Famille
+from django import forms
+
+class MyFamilyAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Famille
+        fields = '__all__'
+
+    def clean_reference(self):
+        return self.cleaned_data['reference'].upper()
+
+    def clean_matiere(self):
+        return self.cleaned_data['matiere'].upper()
 
 
 @admin.register(Famille)
-class MateriauAdmin(admin.ModelAdmin):
-    pass
+class FamilleAdmin(admin.ModelAdmin):
+
+    form = MyFamilyAdminForm
 
 
 @admin.register(SousFamille)
-class MateriauAdmin(admin.ModelAdmin):
-    pass
+class SousFamilleAdmin(admin.ModelAdmin):
+    readonly_fields = ('reference',)
+
 
 
 @admin.register(Materiau)
 class MateriauAdmin(admin.ModelAdmin):
 
     date_hierarchy = 'date_de_creation'
-    readonly_fields = ('qr_code',)
+    readonly_fields = ('qr_code', 'date_de_creation',)
