43e4471646ba646bd7ff8ed615020ff23b18f64e
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-09-26 15:11:07 +0200

    updated migration

--- backend/materiau/migrations/0009_add_default_forme_marchande.py
+++ backend/materiau/migrations/0009_add_default_forme_marchande.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Migrations pour ajouter automatique les formes marchandes prédéfinies
+Migrations pour ajouter automatiquement les formes marchandes prédéfinies en cas de destruction de la DB
 """
 
 from django.db import migrations, models
@@ -14,6 +14,8 @@ def add_default_forme_marchande(apps, schema_editor):
     FormeMarchande.objects.create(type='Plaque')
     FormeMarchande.objects.create(type='Profilé')
     FormeMarchande.objects.create(type='Rouleau')
+    FormeMarchande.objects.create(type='Autre')
+
 
 # reverse function si besoin
 def remove_default_forme_marchande(apps, schema_editor):
@@ -23,6 +25,8 @@ def remove_default_forme_marchande(apps, schema_editor):
     FormeMarchande.objects.get(type='Plaque').delete()
     FormeMarchande.objects.get(type='Profilé').delete()
     FormeMarchande.objects.get(type='Rouleau').delete()
+    FormeMarchande.objects.get(type='Autre').delete()
+
 
 
 class Migration(migrations.Migration):
